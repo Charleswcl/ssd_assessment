37a725cc2c9401c51a6525018cf32ebf3cf0cfc0
Charleswcl <wang90cl@hotmail.com>
2023-03-01 16:55:37 +0800

    first commit

---
+++ src/main/java/batch02/ibf/ssd_assessment/SsdAssessmentApplication.java
@@ -0,0 +1,13 @@
+package batch02.ibf.ssd_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdAssessmentApplication.class, args);
+	}
+
+}
